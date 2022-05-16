5a3aa94c38e1c00642040f974465d90703300132
kitasho0413 <kitasho0413@gmail.com>
2022-05-16 11:02:22 +0000

    8-3

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,6 @@
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
-
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 
@@ -17,4 +16,5 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
 end
