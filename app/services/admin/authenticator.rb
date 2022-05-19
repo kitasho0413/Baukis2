a7265c15ebca2e2d28732b58385e3c1d0c600592
kitasho0413 <kitasho0413@gmail.com>
2022-05-19 13:51:22 +0000

    chapter8 fin

--- app/services/admin/authenticator.rb
+++ app/services/admin/authenticator.rb
@@ -1,11 +1,11 @@
 class Admin::Authenticator
-  def initialize(administor)
-    @admin_member = administor
+  def initialize(administrator)
+    @administrator = administrator
   end
 
   def authenticate(raw_password)
-    @administor &&
-      @administor.hashed_password &&
-      BCrypt::Password.new(@administor.hashed_password) == raw_password
+    @administrator &&
+      @administrator.hashed_password &&
+      BCrypt::Password.new(@administrator.hashed_password) == raw_password
   end
 end
